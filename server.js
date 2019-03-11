7cc93b29f2a23739fde12d26829ea03e1de1a975
Berkowitz <adb16130@d71h76.pr.uconn.edu>
2019-03-11 09:39:47 -0400

    added templating with handlebars

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 require('dotenv').config()
 
 const express = require('express');
+const bodyParser = require('body-parser');
+const hbs = require('express-handlebars');
 
 const routes = require('./routes/routes');
 
@@ -8,8 +10,21 @@ const app = express();
 
 const PORT = process.env.PORT;
 
+app.engine('hbs', hbs({
+  extname: 'hbs',
+  defaultLayout: 'main',
+  layoutsDir: `${__dirname}/views/layouts/`,
+  partialsDir: `${__dirname}/views/partials`
+}))
+
+app.set('view engine', 'hbs');
+
+app.use(express.static('public'))
+app.use(bodyParser.json())
+app.use(bodyParser({ extended: true }))
+
 app.get('/', (req, res) => {
-  res.send('home page')
+  res.render('home', { title: 'Home' })
 })
 
 // delegate API requests to specific routes
